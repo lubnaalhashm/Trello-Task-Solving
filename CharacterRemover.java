a05c6d945c9aba0e85de942fea50cc6783799328
Lubna <lubnaali7912@gmail.com>
2024-09-22 23:18:41 -0800

    Implement the code without referencing an object of the class that I defined.

--- CharacterRemover.java
+++ CharacterRemover.java
@@ -1,8 +1,7 @@
 public class CharacterRemover {
 
-    public String removeCharacter(String str, char ch) {
+    public static String removeCharacter(String str, char ch) {
         StringBuilder result = new StringBuilder();
-
 
         for (int i = 0; i < str.length(); i++) {
             char currentChar = str.charAt(i);
@@ -16,7 +15,6 @@ public class CharacterRemover {
     }
 
     public static void main(String[] args) {
-        CharacterRemover remover = new CharacterRemover();
-        System.out.println(remover.removeCharacter("hello world", 'l'));
+        System.out.println(removeCharacter("hello world", 'l'));
     }
 }
