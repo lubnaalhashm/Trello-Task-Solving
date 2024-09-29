200e2847b5db39a3208eca6b25e1cc4c956b2fe8
Lubna <lubnaali7912@gmail.com>
2024-09-28 22:01:56 -0800

    First commit

--- Maxtriple.java
+++ Maxtriple.java
@@ -1,27 +1,31 @@
 import java.util.Arrays;
 
-public class Maxtriple {
-    public static void main(String[] args) {
-        int[][] arrays = {
-                {8, 10, 9},
-                {6, 9, 3},
-                {5, 2, 8}
-        };
+    import java.util.Arrays;
 
-        for (int[] array : arrays) {
-            System.out.println("Max of " + Arrays.toString(array) + ": " + maxTriple(array));
+    public class Maxtriple {
+        public static void main(String[] args) {
+            int[][] arrays = {
+                    {8, 10, 9},
+                    {6, 9, 3},
+                    {5, 2, 8}
+            };
+
+            for (int[] array : arrays) {
+                System.out.println("Max of " + Arrays.toString(array) + ": " + maxTriple(array));
+            }
+        }
+
+        public static int maxTriple(int[] nums) {
+            if (nums.length >= 1) {
+                int first = nums[0];
+                int middle = nums[nums.length / 2];
+                int last = nums[nums.length - 1];
+
+
+                return Math.max(first, Math.max(middle, last));
+            }
+            return nums[0];
         }
     }
 
-    public static int maxTriple(int[] nums) {
-        if (nums.length >= 1) {
-            int first = nums[0];
-            int middle = nums[nums.length / 2];
-            int last = nums[nums.length - 1];
 
-
-            return Math.max(first, Math.max(middle, last));
-        }
-        return nums[0];
-    }
-}
