9175a40d0fb67ef59e4f8c78ac465d614b901ace
Lubna <lubnaali7912@gmail.com>
2024-09-29 22:40:08 -0800

    modify some errors

--- StringConcatenator.java
+++ StringConcatenator.java
@@ -1,24 +1,36 @@
 public class StringConcatenator {
 
-    // Method to concatenate two strings based on the specified rules
-    public String minCat(String A, String B) {
-        // Find the length of the shorter string
-        int minLength = Math.min(A.length(), B.length());
+    public static int largestOrZero(int a, int b, int c) {
+        int[] nums = {a, b, c};
+        int largest = nums[0];
 
-        // Get the substrings from the end of each string
-        String subA = A.substring(A.length() - minLength);
-        String subB = B.substring(B.length() - minLength);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > largest) {
+                largest = nums[i];
+            }
+        }
 
-        // Concatenate the two substrings
-        return subA + subB;
+        // Check if any two numbers are the same
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return 0;
+                }
+            }
+        }
+
+        return largest;
     }
 
     public static void main(String[] args) {
-        StringConcatenator SC = new StringConcatenator();
-
         // Test cases
-        System.out.println(SC.minCat("Hello", "Hi"));     // Output: "loHi"
-        System.out.println(SC.minCat("Hello", "java"));   // Output: "ellojava"
-        System.out.println(SC.minCat("java", "Hello"));   // Output: "javaello"
+        int[][] testCases = {
+                {3, 2, 5},
+        };
+
+        // Using a for loop
+        for (int[] testCase : testCases) {
+            System.out.println(largestOrZero(testCase[0], testCase[1], testCase[2]));
+        }
     }
 }
