e87cf27289c21254f4d7337aee798a9648577406
Lubna <lubnaali7912@gmail.com>
2024-09-30 01:43:53 -0800

    Change the whole code and use the Scanner package

--- RepeatedCharacter.java
+++ RepeatedCharacter.java
@@ -1,19 +1,21 @@
+import java.util.Scanner;
+
 public class RepeatedCharacter {
 
     public static void main(String[] args) {
-        String str = "swiss";
-        System.out.println("repated char: " +firstRepeatedChar(str));
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the name to find the first repeated character: ");
+        String str = scanner.nextLine();
+        System.out.println("Repeated char: " + firstRepeatedChar(str));
+        scanner.close();
     }
 
     public static Character firstRepeatedChar(String str) {
-        for (int i = 0; i < str.length(); i++) {
-            for (int j = i + 1; j < str.length(); j++) {
-                if (str.charAt(i) == str.charAt(j)) {
-                    return str.charAt(i);
-                }
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                return str.charAt(i);
             }
         }
-        return null; // No repeated character found
+        return null;
     }
 }
