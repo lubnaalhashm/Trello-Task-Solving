9eb02fd3361ac9311d52b3a30bfdbef9ac71fa77
Lubna <lubnaali7912@gmail.com>
2024-09-29 19:43:37 -0800

    Remove Referencing object

---
+++ DigitsSum.java
@@ -0,0 +1,18 @@
+public class DigitsSum {
+
+    public static int sumDigits(String str) {
+        int sum = 0;
+        String[] Array = str.split(""); // Split the string
+        for (String s : Array) {
+            if (Character.isDigit(s.charAt(0))) {
+                sum += Integer.parseInt(s); // Add the digit to the sum
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sumDigits("aa11b33"));
+
+    }
+}
