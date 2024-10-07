052eec1602ca5e79271d54b66e14d98ea9f45ca9
Lubna <lubnaali7912@gmail.com>
2024-10-07 01:16:35 -0800

    The Third commit

--- FrontPiece.java
+++ FrontPiece.java
@@ -3,22 +3,22 @@ import java.util.Arrays;
 public class FrontPiece {
 
     public static void main(String[] args) {
-        int[] Array1 = {7, 8, 9};
-        int[] Array2 = {7, 8};
-        int[] Array3 = {7};
+        int[] array1 = {7, 8, 9};
+        int[] array2 = {7, 8};
+        int[] array3 = {7};
 
         // Test cases
-        printArray(frontPiece(Array1));
-        printArray(frontPiece(Array2));
-        printArray(frontPiece(Array3));
+        printArray(frontPiece(array1));
+        printArray(frontPiece(array2));
+        printArray(frontPiece(array3));
     }
 
     public static int[] frontPiece(int[] arr) {
-        // return the first 2 elements
+        // Return the first 2 elements
         if (arr.length >= 2) {
             return new int[] {arr[0], arr[1]};
         }
-        // return the array itself
+        // Return the array itself
         return arr;
     }
 
