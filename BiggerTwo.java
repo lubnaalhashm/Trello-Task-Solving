ef99d4b51eb86265539506da0eb1f122a040a153
Lubna <lubnaali7912@gmail.com>
2024-10-06 22:02:36 -0800

    The Third commit

--- BiggerTwo.java
+++ BiggerTwo.java
@@ -1,29 +1,25 @@
-public class BiggerTwo {
+import java.util.Arrays;
 
+public class BiggerTwo {
 
     public static void main(String[] args) {
         int[] A = {9, 4};
         int[] B = {7, 1};
 
         // Call the method and display the result
-        int[] result = biggerTwo(A, B);
-        System.out.print("The larger sum: [");
-        System.out.print(result[0] + ", " + result[1]);
-        System.out.println("]");
+        int[] result = findingBiggerTwo(A, B);
+        System.out.println("The larger sum: " + Arrays.toString(result)); //  print the array
     }
 
-    public static int[] biggerTwo(int[] a, int[] b) {
+    public static int[] findingBiggerTwo(int[] a, int[] b) {
         // Calculate the sum of both arrays
         int sumA = a[0] + a[1];
         int sumB = b[0] + b[1];
 
-
         if (sumA >= sumB) {
-            return a;  // Return A in case of a tie or if a has a larger sum
+            return a;  // Return A
         } else {
             return b;
         }
     }
 }
-
-
