aa3fc7a9d832268c9da4f6f9759ee0800c449248
Lubna <lubnaali7912@gmail.com>
2024-10-07 01:11:37 -0800

    The Third commit

--- BoundarySubstring.java
+++ BoundarySubstring.java
@@ -1,6 +1,6 @@
 public class BoundarySubstring {
 
-    public String without2(String str) {
+    public static String without2(String str) {
         int len = str.length();
         if (len == 2 || (len > 2 && str.substring(0, 2).equals(str.substring(len - 2)))) {
             return str.substring(2);
@@ -9,10 +9,9 @@ public class BoundarySubstring {
     }
 
     public static void main(String[] args) {
-        BoundarySubstring handler = new BoundarySubstring();
 
-        System.out.println(handler.without2("HelloHe"));  // Output: "lloHe"
-        System.out.println(handler.without2("HelloHi"));  // Output: "HelloHi"
-        System.out.println(handler.without2("Hi"));       // Output: ""
+        System.out.println(without2("HelloHe"));  // Output: "lloHe"
+        System.out.println(without2("HelloHi"));  // Output: "HelloHi"
+        System.out.println(without2("Hi"));       // Output: ""
     }
 }
