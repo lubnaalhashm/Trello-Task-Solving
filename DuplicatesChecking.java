f45f6fde6cfea60ea705c9a8cc1f0484e0744a20
Lubna <lubnaali7912@gmail.com>
2024-10-08 23:46:51 -0800

    The First commit

---
+++ DuplicatesChecking.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DuplicatesChecking {
+    public static boolean hasDuplicates(List<Integer> nums) {
+        Set<Integer> numbers = new HashSet<>();
+        for (int num : nums) {
+            if (numbers.contains(num)) {
+                return true;
+            }
+            numbers.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasDuplicates(List.of(1, 2, 3, 1)));
+        System.out.println(hasDuplicates(List.of(1, 2, 3)));
+    }
+}
