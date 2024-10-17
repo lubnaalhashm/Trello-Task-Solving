1ea2b858be75a7f0567182a3933ace595cf17e8e
Lubna <lubnaali7912@gmail.com>
2024-10-17 00:12:19 -0800

    Change the class name to the noun

--- DistinctElementsCounter.java
+++ DistinctElementsCounter.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class CountDistinctElements {
+public class DistinctElementsCounter {
     public static void main(String[] args) {
 
         //Declaration and initialization
