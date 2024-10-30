55638a95bec58ee8fc2d60979d7eec789c1b16db
Lubna <lubnaali7912@gmail.com>
2024-10-29 23:26:57 -0800

    adding some comments in the code

--- ArraySplitMultipleConstraints.java
+++ ArraySplitMultipleConstraints.java
@@ -5,17 +5,18 @@ public class ArraySplitMultipleConstraints {
 
     public static void main(String args[]) {
 
-    }
-
+          }
+    //* Method for checking if the array can be split
     public static Boolean splitArray(ArrayList<Integer> nums) {
         return spiltHelperFunction(0, nums, 0, 0);
 
-    }
-
+      }
+    //* Helper function
     public static Boolean spiltHelperFunction(int start, ArrayList<Integer> nums, int sumG1, int sumG2) {
         if (start == nums.size()) {
             return sumG1 == sumG2;
         }
+        //* adding the index from 0
         nums.add(start);
         int currentValue = nums.indexOf(start);
         return spiltHelperFunction(start + 1, nums, sumG1 + currentValue, sumG2) ||
