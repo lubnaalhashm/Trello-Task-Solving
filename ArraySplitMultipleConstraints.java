f15242507e525677da9f82bbe650767cd53380ab
Lubna <lubnaali7912@gmail.com>
2024-10-30 20:40:03 -0800

    first comments

--- ArraySplitMultipleConstraints.java
+++ ArraySplitMultipleConstraints.java
@@ -3,24 +3,27 @@ import java.util.List;
 
 public class ArraySplitMultipleConstraints {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
-          }
+        //* Calling methods and print the output
+        System.out.println(splitArray(new ArrayList<>(List.of(5 ,2 , 3 )))); // true
+    }
+
     //* Method for checking if the array can be split
     public static Boolean splitArray(ArrayList<Integer> nums) {
-        return spiltHelperFunction(0, nums, 0, 0);
+        return splitHelperFunction(0, nums, 0, 0);
+    }
 
-      }
     //* Helper function
-    public static Boolean spiltHelperFunction(int start, ArrayList<Integer> nums, int sumG1, int sumG2) {
+    public static Boolean splitHelperFunction(int start, ArrayList<Integer> nums, int sumG1, int sumG2) {
+        //* base case
         if (start == nums.size()) {
             return sumG1 == sumG2;
         }
-        //* adding the index from 0
-        nums.add(start);
-        int currentValue = nums.indexOf(start);
-        return spiltHelperFunction(start + 1, nums, sumG1 + currentValue, sumG2) ||
-                spiltHelperFunction(start + 1, nums, sumG1, sumG2 + currentValue);
-    }
 
+        int currentValue = nums.get(start);
+
+        return splitHelperFunction(start + 1, nums, sumG1 + currentValue, sumG2) ||
+                splitHelperFunction(start + 1, nums, sumG1, sumG2 + currentValue);
+    }
 }
